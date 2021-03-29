c5a9b521ac4e40ba5e0dc8d1071ab8b77151fc19
Taylor <greatwall1001@tamu.edu>
2021-03-29 12:56:34 -0500

    Update to benchmark 2. g

--- Benchmark2.bat
+++ Benchmark2.bat
@@ -1,3 +1,5 @@
 @REM build\Release\Benchmark_5_1.exe 10 Benchmark_5_1_DebuggingError E:\Experiments\03_04_2021\Benchmark_5_1_DebuggingError\ 1 1 20 10 5 1 > Benchmark_5_1_DebuggingError.txt
 
-build\Release\Benchmark_5_1.exe 10 Benchmark_5_1_Normals_Arclengths_5_5 E:\Experiments\03_04_2021\Benchmark_5_1_Normals_Arclengths_5_5\ 1 1 20 10 5 1 5 > Benchmark_5_1_Normals_Arclengths_5_5.txt
+@REM build\Release\Benchmark_5_1.exe 10 Benchmark_5_1_Normals_Arclengths_5_5 F:\Experiments\03_04_2021\Benchmark_5_1_Normals_Arclengths_5_5\ 1 1 20 10 5 1 5 > Benchmark_5_1_Normals_Arclengths_5_5.txt
+
+build\Release\Benchmark_5_1.exe 10 Benchmark_5_1_Normals_Arclengths_10_10 F:\Experiments\03_09_2021\Benchmark_5_1_Normals_Arclengths_10_10\ 1 1 20 10 10 1 10 > Benchmark_5_1_Normals_Arclengths_10_10.txt
