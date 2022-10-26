c3c33e8defdf8c3843db877679cdf28964e55ea1
BrennenTaylor <brtaylor1001@gmail.com>
2022-10-26 12:28:29 -0500

    UpdatE

--- Neural/test_sph.cpp
+++ Neural/test_sph.cpp
@@ -6,10 +6,11 @@
 #include <cmath>
 #include <vector>
 #include <random>
+#include <assert.h>
 
 constexpr uint32_t numBands = 4;
 constexpr uint32_t NumCoeff = numBands + ((numBands - 1) * (numBands - 1)) + (numBands - 1);
-constexpr uint32_t numSamplesSqrt = 5000;
+constexpr uint32_t numSamplesSqrt = 2000;
 
 struct SPHSample {
     Farlor::Vector2 sph = Farlor::Vector2(0.0f, 0.0f);
@@ -46,8 +47,11 @@ std::vector<float> ProjectPolarFunction(const std::vector<SPHSample> &samples)
     return result;
 }
 
+// both l and m are positive integers
 float P(int l, int m, float x)
 {
+    assert(l >= 0);
+    assert(m >= 0);
     float ppm = 1.0f;
     if (m > 0) {
         float somx2 = std::sqrt((1.0f - x) * (1.0f + x));
@@ -61,13 +65,15 @@ float P(int l, int m, float x)
     if (l == m)
         return ppm;
 
-    float ppmp1 = x * (2.0f * m + 1.0f) * ppm;
+    float ppmp1 = x * (2.0f * static_cast<float>(m) + 1.0f) * ppm;
     if (l == m + 1)
         return ppmp1;
 
     float pll = 0.0f;
     for (int ll = m + 2; ll <= l; ++ll) {
-        pll = ((2.0f * ll - 1.0f) * x * ppmp1 - (ll + m - 1.0f) * ppm) / (ll - m);
+        pll = ((2.0f * static_cast<float>(ll) - 1.0f) * x * ppmp1
+                    - (static_cast<float>(ll) + static_cast<float>(m) - 1.0f) * ppm)
+              / (static_cast<float>(ll) - static_cast<float>(m));
         ppm = ppmp1;
         ppmp1 = pll;
     }
@@ -77,20 +83,23 @@ float P(int l, int m, float x)
 // Renomalization constant for SH
 float K(int l, int m)
 {
-    float temp = ((2.0f * l + 1.0f) * std::tgammaf(l - m + 1))
-          / (4.0f * std::numbers::pi_v<float> * std::tgammaf(l + m + 1));
+    float temp = ((2.0f * static_cast<float>(l) + 1.0f)
+                       * std::tgamma(static_cast<float>(l - abs(m)) + 1.0f))
+          / (4.0f * std::numbers::pi_v<float> * std::tgamma(static_cast<float>(l + abs(m)) + 1.0f));
     return std::sqrtf(temp);
 }
 
+// l is 0 - > inf
+// m is [-l, l]
 float SH(int l, int m, float theta, float phi)
 {
     const float sqrt2 = std::sqrtf(2.0f);
     if (m == 0) {
-        return K(l, 0) * P(l, m, cos(theta));
+        return K(l, 0) * P(l, 0, cos(theta));
     } else if (m > 0) {
-        return sqrt2 * K(l, m) * cos(m * phi) * P(l, m, std::cos(theta));
+        return sqrt2 * K(l, m) * cos(static_cast<float>(m) * phi) * P(l, m, std::cos(theta));
     } else {
-        return sqrt2 * K(l, -m) * std::sin(-m * phi) * P(l, -m, std::cos(theta));
+        return sqrt2 * K(l, m) * std::sin(static_cast<float>(-m) * phi) * P(l, -m, std::cos(theta));
     }
 }
 
@@ -113,7 +122,6 @@ int main()
         sample.sph.x = 2.0f * std::acos(std::sqrt(1.0f - e0));
         sample.sph.y = 2.0f * std::numbers::pi_v<float> * e1;
 
-
         // Precompute coefficients
         for (int l = 0; l < numBands; l++) {
             for (int m = -l; m <= l; m++) {
@@ -121,12 +129,6 @@ int main()
                 sample.coeff[sphIdx] = SH(l, m, sample.sph.x, sample.sph.y);
             }
         }
-
-        // std::cout << "Sample coefficients: " << std::endl;
-        // for (auto &coefficient : sample.coeff) {
-        //     std::cout << coefficient << ", ";
-        // }
-        // std::cout << std::endl;
     }
 
     std::vector<float> coefficients = ProjectPolarFunction(samples);
@@ -144,6 +146,38 @@ int main()
             numNextLine += 2;
         }
     }
+
+
+    // Lets reconstruct the signal
+    std::vector<SPHSample> testSamples(20);
+    for (uint32_t sampleIdx = 0; sampleIdx < numSamplesSqrt * numSamplesSqrt; sampleIdx++) {
+        const uint32_t a = sampleIdx / numSamplesSqrt;
+        const uint32_t b = sampleIdx % numSamplesSqrt;
+
+        const float oneOverSqrtN = 1.0f / numSamplesSqrt;
+        const float e0 = (a + uniform01(generator)) * oneOverSqrtN;
+        const float e1 = (b + uniform01(generator)) * oneOverSqrtN;
+
+
+        auto &sample = samples[sampleIdx];
+        sample.sph.x = 2.0f * std::acos(std::sqrt(1.0f - e0));
+        sample.sph.y = 2.0f * std::numbers::pi_v<float> * e1;
+
+        float reconstructedValue = 0.0f;
+        // Precompute coefficients
+        for (int l = 0; l < numBands; l++) {
+            for (int m = -l; m <= l; m++) {
+                int sphIdx = l * (l + 1) + m;
+                reconstructedValue += coefficients[sphIdx] * SH(l, m, sample.sph.x, sample.sph.y);
+            }
+        }
+        const float exactValue = LightFunction(sample.sph.x, sample.sph.y);
+
+        std::cout << "Reconstructed: " << reconstructedValue << "\n";
+        std::cout << "Exact value: " << exactValue << "\n";
+        std::cout << "Abs Error: " << abs(reconstructedValue - exactValue) << std::endl;
+    }
+
     std::cout << std::endl;
 
     return 0;
