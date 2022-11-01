adf123ae7aa93cae79a32bd896e6ead7bcae8a9d
BrennenTaylor <brtaylor1001@gmail.com>
2022-11-01 17:12:18 -0500

    Update with correct nn implementation. First stab, generates network. Lets try to evaluate next

--- Neural/python/neural_network.py
+++ Neural/python/neural_network.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division
 import os
+import math
 import torch
 import torchvision
 import pandas as pd
@@ -14,10 +15,30 @@ from torch.utils.tensorboard import SummaryWriter
 
 class VolumeScatterDataset(Dataset):
 
-    def __init__(self, filename, root_dir, transform=None):
-        self.loaded_data = pd.read_csv(filename, header=None)
+    def __init__(self, filename, root_dir):
         self.root_dir = root_dir
-        self.transform = transform
+        read_data = pd.read_csv(filename, header=None).to_numpy()
+
+
+        scaled_data = np.zeros((read_data.shape[0], 12))
+        
+        scaled_data[:, 0] = read_data[:, 0] / 10
+        scaled_data[:, 1] = read_data[:, 1] / 10
+        scaled_data[:, 2] = read_data[:, 2] / 10
+
+        scaled_data[:, 3] = np.sin(read_data[:, 3]) * np.cos(read_data[:, 4])
+        scaled_data[:, 4] = np.sin(read_data[:, 3]) * np.sin(read_data[:, 4])
+        scaled_data[:, 5] = np.cos(read_data[:, 3])
+ 
+        scaled_data[:, 6] = read_data[:, 5] / 20
+        scaled_data[:, 7] = read_data[:, 6] / 20
+        scaled_data[:, 8] = read_data[:, 7] / 20
+
+        scaled_data[:, 9] = read_data[:, 8]
+        scaled_data[:, 10] = read_data[:, 9]
+        scaled_data[:, 11] = read_data[:, 10]
+
+        self.loaded_data = scaled_data
 
     def __len__(self):
         return len(self.loaded_data)
@@ -26,17 +47,11 @@ class VolumeScatterDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        input_vector = self.loaded_data.iloc[idx, 0:8]
+        input_vector = self.loaded_data[idx][0:9]
         input_vector = np.array([input_vector], dtype=float)
 
-        radiance = self.loaded_data.iloc[idx, 8:11]
+        radiance = self.loaded_data[idx,][9:12]
         radiance = np.array([radiance], dtype=float)
-
-        if self.transform:
-            input_vector = self.transform(input_vector)
-
-        if self.transform:
-            radiance = self.transform(radiance)
 
         return input_vector, radiance
 
@@ -45,36 +60,46 @@ class VolumeScatterModel(torch.nn.Module):
     def __init__(self):
         super(VolumeScatterModel, self).__init__()
 
-        self.linear1 = torch.nn.Linear(8, 200)
         self.activation = torch.nn.ReLU()
-        self.linear2 = torch.nn.Linear(200, 3)
-        self.softmax = torch.nn.Softmax()
+        self.linear1 = torch.nn.Linear(9, 512)
+        self.linear2 = torch.nn.Linear(512, 512)
+        self.linear3 = torch.nn.Linear(512, 512)
+        self.linear4 = torch.nn.Linear(512, 512)
+        self.linear5 = torch.nn.Linear(512, 3)
 
     def forward(self, x):
         x = self.linear1(x)
         x = self.activation(x)
         x = self.linear2(x)
-        x = self.softmax(x)
+        x = self.activation(x)
+        x = self.linear3(x)
+        x = self.activation(x)
+        x = self.linear4(x)
+        x = self.activation(x)
+        x = self.linear5(x)
+        x = self.activation(x)
         return x
 
 
 dtype = torch.float
-device = torch.device("cpu")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.backends.cudnn.benchmark = True
+print(device)
 
 model = VolumeScatterModel()
+model.to(device)
 
-loss_fn = torch.nn.MSELoss(reduction='sum')
-learning_rate = 1e-6
+loss_fn = torch.nn.MSELoss(reduction='mean')
 
 
 train_dataset = VolumeScatterDataset(filename='dataset/single_scatter_raymarch/samples.csv', root_dir='dataset/single_scatter_raymarch/')
-train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=1024, shuffle=True, pin_memory=True)
 
 validation_dataset = VolumeScatterDataset(filename='dataset/single_scatter_raymarch/validation.csv', root_dir='dataset/single_scatter_raymarch/')
-validation_dataloader = DataLoader(validation_dataset, batch_size=64, shuffle=True)
+validation_dataloader = DataLoader(validation_dataset, batch_size=1024, shuffle=True, pin_memory=True)
 
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 def train_one_epoch(epoch_index, tb_writer):
     running_loss = 0.
@@ -87,9 +112,11 @@ def train_one_epoch(epoch_index, tb_writer):
         
         train_input, train_output = data
         
-        train_input = train_input.float()
-        train_output = train_output.float()
+        train_input = train_input.float().to(device)
+        train_output = train_output.float().to(device)
 
+
+        # import pdb; pdb.set_trace()
         # Zero your gradients for every batch!
         optimizer.zero_grad()
 
@@ -98,6 +125,7 @@ def train_one_epoch(epoch_index, tb_writer):
 
         # Compute the loss and its gradients
         loss = loss_fn(model_prediction, train_output)
+        # print(loss)
         loss.backward()
 
         # Adjust learning weights
@@ -105,8 +133,8 @@ def train_one_epoch(epoch_index, tb_writer):
 
         # Gather data and report
         running_loss += loss.item()
-        if i % 1000 == 999:
-            last_loss = running_loss / 1000 # loss per batch
+        if i % 10 == 0:
+            last_loss = running_loss / 10 # loss per batch
             print('  batch {} loss: {}'.format(i + 1, last_loss))
             tb_x = epoch_index * len(train_dataloader) + i + 1
             tb_writer.add_scalar('Loss/train', last_loss, tb_x)
@@ -135,11 +163,11 @@ for epoch in range(EPOCHS):
     model.train(False)
 
     running_vloss = 0.0
-    for i, vdata in enumerate(validation_loader):
+    for i, vdata in enumerate(validation_dataloader):
         vinputs, vGT = vdata
 
-        vinputs = vinputs.float()
-        vGT = vGT.float()
+        vinputs = vinputs.float().to(device)
+        vGT = vGT.float().to(device)
 
         voutputs = model(vinputs)
         vloss = loss_fn(voutputs, vGT)
@@ -160,5 +188,8 @@ for epoch in range(EPOCHS):
         best_vloss = avg_vloss
         model_path = 'model_{}_{}'.format(timestamp, epoch_number)
         torch.save(model.state_dict(), model_path)
+
+        
+        # torch.onnx.export(model, "alexnet.onnx", verbose=True, input_names=input_names, output_names=output_names)
 
     epoch_number += 1
